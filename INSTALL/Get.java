9c9899269b2d12b22fdc03022e155bb067ae24cc
wangshuai-lu <3011608365@qq.com>
2022-06-29 19:44:26 +0800

    添加：判断excel的xlsx中是否有图片

---
+++ INSTALL/Get.java
@@ -0,0 +1,27 @@
+
+import org.apache.poi.xssf.usermodel.*;
+import java.io.FileInputStream;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Get {
+
+
+    public static void main(String[] args)throws Exception {
+        Scanner sc=new Scanner(System.in);
+        InputStream inp = new FileInputStream(sc.next());
+        XSSFWorkbook workbook = new XSSFWorkbook(inp);//读取现有的Excel文件
+        List<XSSFPictureData> pictures = workbook.getAllPictures();
+
+            if (pictures.size()!=0)//判断文件格式
+            {
+                System.out.println("该Excel有.png图片");
+            }
+            if(pictures.size() ==0)
+            {
+                System.out.println("该Excel无.png图片");
+            }
+
+
+    }
+}
